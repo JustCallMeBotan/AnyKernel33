869e3c172422a83cb820f38c699c3bdcb0d61da8
Botan <botan@kucinghitam.pp.ua>
2026-02-24 11:45:06 +0700

    change script anykernel

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=LineagePlus Kernel for Xiaomi MT6893 Series
+kernel.string=KucingHitam Kernel OSS for chopin
 do.devicecheck=1
 do.modules=0
 do.systemless=1
